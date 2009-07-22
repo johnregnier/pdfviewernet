54dc09a1bbef926b30fc2b89274cc54e952b3c8b
snakebyteme2 <snakebyteme2@3774bada-5c05-11de-8888-c79ed7c8b0c3>
2009-07-22 12:13:25 +0000

    Added ability to open encrypted PDF files. Added ability to re-open the PDF with GhostScript if XPDF fails

---
+++ trunk/PDFView/PDFView/Password.vb
@@ -0,0 +1,21 @@
+﻿Public Class Password
+
+    Public UserPassword As String = ""
+    Public OwnerPassword As String = ""
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub UsernameTextBox_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles UsernameTextBox.TextChanged
+        UserPassword = UsernameTextBox.Text
+    End Sub
+
+    Private Sub PasswordTextBox_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles PasswordTextBox.TextChanged
+        OwnerPassword = PasswordTextBox.Text
+    End Sub
+End Class
